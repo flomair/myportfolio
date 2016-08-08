f870532b68f79398eb75a7dd0358c37bce4baad5
Florian Mair <f.mair@gmx.net>
2016-08-09 00:39:42 +0200

    extend load config from excel, split excel sheets to jsonfiles for angular acces, img responsive resize from config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,6 +13,9 @@ import excelsheets2json from 'gulp-excelsheets2json';
 import ext_replace from 'gulp-ext-replace';
 import router from 'front-router';
 import ngHtml2Js from 'gulp-ng-html2js';
+import rename from 'gulp-rename';
+import imageResize from 'gulp-image-resize';
+import toJson from 'gulp-to-json';
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
@@ -20,171 +23,198 @@ const $ = plugins();
 const PRODUCTION = !!(yargs.argv.production);
 
 // Load settings from settings.yml
-const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
+const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS, RESP } = loadConfig();
 
 function loadConfig() {
     let ymlFile = fs.readFileSync('config.yml', 'utf8');
     return yaml.load(ymlFile);
 }
 
+
+var resolutions;
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
-    gulp.series(clean, gulp.parallel(index, pages, sass, javascript,templates, angular,jslib, images, copy, ex2json)));
+    gulp.series(clean, ex2json , gulp.parallel(index, pages, sass, javascript, templates, angular, jslib, images,resp, copy)));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
     gulp.series('build', server, watch));
 
-// Delete the "dist" folder
-// This happens every time a build starts
-function clean(done) {
-    rimraf(PATHS.dist, done);
-}
-
-// Copy files out of the assets folder
-// This task skips over the "img", "js", and "scss" folders, which are parsed separately
-function copy() {
-    return gulp.src(PATHS.assets)
-        .pipe(gulp.dest(PATHS.dist + '/assets'));
-}
-
-function ex2json() {
-    return gulp.src(PATHS.xlsx)
-        .pipe(excelsheets2json({
-            dest: PATHS.dist + '/assets/data'
-        }))
-        //.pipe(ext_replace('.json'))
-        //.pipe(gulp.dest(PATHS.dist + '/assets/data'))
-}
-
-// Copy page templates into finished HTML files
-function pages(done) {
-    gulp.src('./src/templates/**/*.html')
-        .pipe(router({
-            path: './dist/assets/js/routes.js',
-            root: 'src'
-        }))
-        .pipe(gulp.dest(PATHS.dist + '/templates'))
-    done();
-}
-
-// Load updated HTML templates and partials into Panini
-function resetPages(done) {
-    panini.refresh();
-    done();
-}
-
-// Generate a style guide from the Markdown content and HTML template in styleguide/
-function index() {
-       return gulp.src('./src/index.html')
-        .pipe(gulp.dest(PATHS.dist))
-}
-
-// Compile Sass into CSS
-// In production, the CSS is compressed
-function sass() {
-    return gulp.src('src/assets/scss/app.scss')
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({
-                includePaths: PATHS.sass
-            })
-            .on('error', $.sass.logError))
-        .pipe($.autoprefixer({
-            browsers: COMPATIBILITY
-        }))
-        // Comment in the pipe below to run UnCSS in production
-        //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
-        .pipe($.if(PRODUCTION, $.cssnano()))
-        .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-        .pipe(gulp.dest(PATHS.dist + '/assets/css'))
-        .pipe(browser.reload({ stream: true }));
-}
-
-// Combine JavaScript into one file
-// In production, the file is minified
-function javascript() {
-    return gulp.src('src/assets/js/*.js')
-        //.pipe($.sourcemaps.init())
-        //.pipe($.babel())
-        //.pipe($.concat('appfou.js'))
-        .pipe($.if(PRODUCTION, $.uglify()
-            .on('error', e => { console.log(e); })
-        ))
-        //.pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-        .pipe(gulp.dest(PATHS.dist + '/assets/js'));
-}
-
-function jslib() {
-    return gulp.src(PATHS.javascript)
-        .pipe($.sourcemaps.init())
-        .pipe($.babel())
-        .pipe($.concat('appfou.js'))
-        .pipe($.if(PRODUCTION, $.uglify()
-            .on('error', e => { console.log(e); })
-        ))
-        .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-        .pipe(gulp.dest(PATHS.dist + '/assets/js'));
-}
-
-function angular() {
-    return gulp.src(PATHS.foundationJS)
-        .pipe($.concat('foundation.js'))
-        .pipe(gulp.dest(PATHS.dist + '/assets/js'));
-}
-function templates(done) {
-    gulp.src('bower_components/foundation-apps/js/angular/components/**/*.html')
-        .pipe(ngHtml2Js({
-            prefix: 'components/',
-            moduleName: 'foundation',
-            declareModule: false
-        }))
-        .pipe($.uglify())
-        .pipe($.concat('templates.js'))
-       .pipe(gulp.dest(PATHS.dist + '/assets/js'));
-
-
-    // Iconic SVG icons
-    gulp.src('./bower_components/foundation-apps/iconic/**/*')
-        .pipe(gulp.dest(PATHS.dist + '/assets/img/iconic/'));
-  done();
-}
 
 
 
+        // Copy images to the "dist" folder
+    // In production, the images are compressed
+    function images() {
+        return gulp.src('src/assets/img/**/*')
+            .pipe($.if(PRODUCTION, $.imagemin({
+                progressive: true
+            })))
+            .pipe(gulp.dest(PATHS.dist + '/assets/img'));
+    }
 
-// Copy images to the "dist" folder
-// In production, the images are compressed
-function images() {
-    return gulp.src('src/assets/img/**/*')
-        .pipe($.if(PRODUCTION, $.imagemin({
-            progressive: true
-        })))
-        .pipe(gulp.dest(PATHS.dist + '/assets/img'));
-}
+    function getresolutions(){
+       return (JSON.parse(fs.readFileSync('dist/assets/data/config.json', 'utf8'))).resp;
+    }
 
-// Start a server with BrowserSync to preview the site in
-function server(done) {
-    browser.init({
-        server: PATHS.dist,
-        port: PORT
-    });
-    done();
-}
 
-// Reload the browser with BrowserSync
-function reload(done) {
-    browser.reload();
-    done();
-}
+    function resp(done) {
+        rimraf(PATHS.dist+'/assets/imgs', done);
+        var  resps = getresolutions();
+        resps.forEach(function (co) {
+                gulp.src("./src/assets/img/*.jpg")
+                    .pipe(
+                        imageResize({
+                        width: co.width
+                    }))
+                    .pipe($.if(PRODUCTION, $.imagemin({
+                        progressive: true
+                    })))
+                    .pipe(rename({suffix: co.suffix}))
+                    .pipe(gulp.dest(PATHS.dist+'/assets/img'))
+        });
+        done();
+    };
 
-// Watch for changes to static assets, pages, Sass, and JavaScript
-function watch() {
-    gulp.watch(PATHS.assets, copy);
-    gulp.watch('src/templates/**/*.html').on('change', gulp.series(pages, browser.reload));
-    gulp.watch('src/index.html').on('change', gulp.series(index, browser.reload));
-    gulp.watch('src/assets/scss/**/*.scss', sass);
-    gulp.watch('src/assets/js/**/*.js').on('change', gulp.series(javascript, browser.reload));
-    gulp.watch('src/assets/img/**/*').on('change', gulp.series(images, browser.reload));
-    gulp.watch('src/assets/data/*.xls').on('change', gulp.series(ex2json, browser.reload));
-    // gulp.watch('src/styleguide/**').on('change', gulp.series(styleGuide, browser.reload));
-}
+    // Delete the "dist" folder
+    // This happens every time a build starts
+    function clean(done) {
+        rimraf(PATHS.dist, done);
+    }
+
+    // Copy files out of the assets folder
+    // This task skips over the "img", "js", and "scss" folders, which are parsed separately
+    function copy() {
+        return gulp.src(PATHS.assets)
+            .pipe(gulp.dest(PATHS.dist + '/assets'));
+    }
+
+    function ex2json(done) {
+            excelsheets2json(__dirname+"/"+PATHS.xlsx,{dest: PATHS.dist + '/assets/data'});
+            done();
+    }
+
+    // Copy page templates into finished HTML files
+    function pages(done) {
+        gulp.src('./src/templates/**/*.html')
+            .pipe(router({
+                path: './dist/assets/js/routes.js',
+                root: 'src'
+            }))
+            .pipe(gulp.dest(PATHS.dist + '/templates'))
+        done();
+    }
+
+    // Load updated HTML templates and partials into Panini
+    function resetPages(done) {
+        panini.refresh();
+        done();
+    }
+
+    // Generate a style guide from the Markdown content and HTML template in styleguide/
+    function index() {
+        return gulp.src('./src/index.html')
+            .pipe(gulp.dest(PATHS.dist))
+    }
+
+    // Compile Sass into CSS
+    // In production, the CSS is compressed
+    function sass() {
+        return gulp.src('src/assets/scss/app.scss')
+            .pipe($.sourcemaps.init())
+            .pipe($.sass({
+                    includePaths: PATHS.sass
+                })
+                .on('error', $.sass.logError))
+            .pipe($.autoprefixer({
+                browsers: COMPATIBILITY
+            }))
+            // Comment in the pipe below to run UnCSS in production
+            //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+            .pipe($.if(PRODUCTION, $.cssnano()))
+            .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+            .pipe(gulp.dest(PATHS.dist + '/assets/css'))
+            .pipe(browser.reload({ stream: true }));
+    }
+
+    // Combine JavaScript into one file
+    // In production, the file is minified
+    function javascript() {
+        return gulp.src('src/assets/js/*.js')
+            //.pipe($.sourcemaps.init())
+            //.pipe($.babel())
+            //.pipe($.concat('appfou.js'))
+            .pipe($.if(PRODUCTION, $.uglify()
+                .on('error', e => { console.log(e); })
+            ))
+            //.pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+            .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+    }
+
+    function jslib() {
+        return gulp.src(PATHS.javascript)
+            .pipe($.sourcemaps.init())
+            .pipe($.babel())
+            .pipe($.concat('appfou.js'))
+            .pipe($.if(PRODUCTION, $.uglify()
+                .on('error', e => { console.log(e); })
+            ))
+            .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+            .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+    }
+
+    function angular() {
+        return gulp.src(PATHS.foundationJS)
+            .pipe($.concat('foundation.js'))
+            .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+    }
+
+    function templates(done) {
+        gulp.src('bower_components/foundation-apps/js/angular/components/**/*.html')
+            .pipe(ngHtml2Js({
+                prefix: 'components/',
+                moduleName: 'foundation',
+                declareModule: false
+            }))
+            .pipe($.uglify())
+            .pipe($.concat('templates.js'))
+            .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+
+
+        // Iconic SVG icons
+        gulp.src('./bower_components/foundation-apps/iconic/**/*')
+            .pipe(gulp.dest(PATHS.dist + '/assets/img/iconic/'));
+        done();
+    }
+
+
+
+
+
+
+    // Start a server with BrowserSync to preview the site in
+    function server(done) {
+        browser.init({
+            server: PATHS.dist,
+            port: PORT
+        });
+        done();
+    }
+
+    // Reload the browser with BrowserSync
+    function reload(done) {
+        browser.reload();
+        done();
+    }
+
+    // Watch for changes to static assets, pages, Sass, and JavaScript
+    function watch() {
+        gulp.watch(PATHS.assets, copy);
+        gulp.watch('src/templates/**/*.html').on('change', gulp.series(pages, browser.reload));
+        gulp.watch('src/index.html').on('change', gulp.series(index, browser.reload));
+        gulp.watch('src/assets/scss/**/*.scss', sass);
+        gulp.watch('src/assets/js/**/*.js').on('change', gulp.series(javascript, browser.reload));
+        gulp.watch(['src/assets/img/**/*', 'src/assets/data/resolutions.json']).on('change', gulp.series(resp, browser.reload));
+        gulp.watch(['src/assets/data/projects3.xls', 'src/assets/data/resolutions.json']).on('change', gulp.series(ex2json, browser.reload));
+        // gulp.watch('src/styleguide/**').on('change', gulp.series(styleGuide, browser.reload));
+    }
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 (function () {
     'use strict';
 
-    angular.module('application', [
+    var myApplication = angular.module('application', [
             'ui.router',
             'ngAnimate',
 
@@ -19,28 +19,34 @@ $(document).foundation();
             $scope.lastname = "dada";
         })
         .controller('allprojectsCtrl', function ($scope, $http) {
-            $http.get('/assets/data/main.json', { cache: true })
+            $http.get('/assets/data/main.json', { cache: false })
                 .then(function (res) {
-                    console.log(res.data);
                     $scope.projects = res.data;
+
+                    for(var d in $scope.projects.dets){
+                      $scope.projects.dets[d].resps =$scope.projects.dets[d].imgs["main-img"].resps;
+                      $scope.projects.dets[d].ialt =$scope.projects.dets[d].imgs["main-img"].alt;
+                    console.log($scope.projects);
+                    }
                 });
 
         })
 
-        .controller('detailsCrtl', function ($scope, $http, $stateParams) {
-            $http.get('/assets/data/'+$stateParams.id+'.json', { cache: true })
-                .then(function (res) {
-                    $scope.project = res.data;
+    .controller('detailsCrtl', function ($scope, $http, $stateParams) {
+        $http.get('/assets/data/' + $stateParams.id + '.json', { cache: false })
+            .then(function (res) {
+                $scope.project = res.data;
 
-                $http.get('/assets/data/main.json', { cache: true })
-                .then(function (res) {
-                    $scope.project.second = res.data;
-                    console.log($scope.project);
-                });
+                $http.get('/assets/data/main.json', { cache: false })
+                    .then(function (res) {
+                        $scope.project.second = res.data;
+                        //console.log($scope.project);
+                    });
 
             });
-        });
+    });
 
+    fetchData().then(bootstrapApplication);
 
 
     config.$inject = ['$urlRouterProvider', '$locationProvider'];
@@ -60,5 +66,162 @@ $(document).foundation();
         FastClick.attach(document.body);
     }
 
+
+
+    function fetchData() {
+        var initInjector = angular.injector(["ng"]);
+        var $http = initInjector.get("$http");
+
+        return $http.get("/assets/data/config.json").then(function(response) {
+            myApplication.constant("config", response.data);
+        }, function(errorResponse) {
+            // Handle error case
+        });
+    }
+
+    function bootstrapApplication() {
+        angular.element(document).ready(function() {
+            angular.bootstrap(document, ["application"]);
+        });
+    }
+
+
+
+
+
+
+
+
+
+
 })();
 
+/*
+(function () {
+    'use strict';
+
+
+
+    var toJson = function (workbook, resp) {
+        var result = {},
+            re, i,config;
+        workbook.SheetNames.sort(function (a, b) {
+          if(a === "config"){
+              return -1;
+          }
+          if (b === "config") {
+            return 1;
+         }
+          return 0;
+        });
+        workbook.SheetNames.forEach(function (sheetName) {
+            var roa = XLS.utils.sheet_to_row_object_array(workbook.Sheets[sheetName], { "header": 0 });
+            if (roa.filter(function(el){return el.oname === "#datasbeginn"}).length === 0) {
+                if (roa.length > 0) {
+                    re = { "id": sheetName };
+                    for (i = 0; i < roa.length; i++) {
+                        if (roa[i].oname != "undefined") {
+                            try {
+                                re[roa[i].oname] = JSON.parse(roa[i].ovalue);
+                            } catch (err) {
+                                re[roa[i].oname] = roa[i].ovalue;
+                            }
+                        }
+                    }
+                    if(re.imgs){
+                    re.imgs = setresps(re.imgs,resp);
+                    }
+                }
+            } else {
+                re = { "dets": {} };
+                var detsarr = [];
+                var detsarr2 = [];
+                for (i = 0; i < roa.length; i++) {
+                    if (roa[i].oname) {
+                        if (roa[i].oname === "#datasbeginn") break;
+                        try {
+                            re[roa[i].oname] = JSON.parse(roa[i].ovalue);
+                        } catch (err) {
+                            re[roa[i].oname] = roa[i].ovalue;
+                        }
+                    }
+                }
+                for (var de in roa[i]) {
+                    if (de != "oname") {
+                        re.dets[roa[i][de]] = { "id": roa[i][de] };
+                        detsarr.push(de);
+                        detsarr2.push(roa[i][de]);
+                    }
+                }
+                for (var j = i + 1; i < roa.length; j++) {
+                    if (roa[i].oname != "undefined") {
+                        if (roa[j].oname === "#datasend") break;
+                        for (var d = 0; d < detsarr.length; d++) {
+                            try {
+                                re.dets[detsarr2[d]][roa[j].oname] = JSON.parse(roa[j][detsarr[d]]);
+                            } catch (err) {
+                                re.dets[detsarr2[d]][roa[j].oname] = roa[j][detsarr[d]];
+                            }
+                        }
+                    }
+
+                }
+
+                for(var i in re.dets){
+                    if(re.dets[i].imgs){
+                        re.dets[i].imgs = setresps(re.dets[i].imgs,resp);
+                    }
+                }
+
+                //re.imgs = setresps(re.imgs,resp);
+            }
+            if(sheetName === "config"){
+                config = re;
+            }
+            console.log(re);
+            //createFile(dest+"/"+sheetName+".json", JSON.stringify(re),function(err){});
+            //fs.writeFile(dest+"/"+sheetName+".json", JSON.stringify(re),function(err){});
+            result[sheetName] = re;
+        });
+
+        //createFile(dest+"/complete.json", JSON.stringify(result),function(err){});
+        //fs.writeFile(dest+"/complete.json", JSON.stringify(result),function(err){});
+        return result;
+    }
+
+
+
+    var url = "/assets/data/projects3.xls";
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    oReq.onload = function (e) {
+        var arraybuffer = oReq.response;
+        var data = new Uint8Array(arraybuffer);
+        var arr = new Array();
+        for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+        var bstr = arr.join("");
+
+        $.getJSON("/assets/data/resolutions.json", function (data) {
+            var workbook = XLS.read(bstr, { type: "binary" });
+            toJson(workbook, data);
+        });
+    }
+
+    oReq.send();
+
+
+
+
+
+    function setresps(r,re) {
+        for (var i in r) {
+            r[i].resps = {};
+            re.forEach(function (co) {
+                r[i].resps[co.width] = r[i].img + co.suffix + "." + r[i].ext;
+            });
+        }
+        return r;
+    }
+})();
+*/
